f565ac93c70e863ca29ec56ff4f3f9ee3c016032
Mitok Oleksandr <mitok.alexander@gmail.com>
2022-02-14 13:41:05 +0200

    fix

--- templates/azuredeploy.ps1
+++ templates/azuredeploy.ps1
@@ -1,7 +1,9 @@
 param($Location, $ResourceGroupName, $DatabasePassword, $TemplateFile, $TemplateParameterFile, $prefix, $slackURL)
+#$TemplateFile="templates\azuredeploy.json"
+#$TemplateParameterFile="templates\azuredeploy.parameters.json"
 
-$TemplateFile="templates\azuredeploy.json"
-$TemplateParameterFile="templates\azuredeploy.parameters.json"
+$TemplateFile=".\azuredeploy.json"
+$TemplateParameterFile=".\azuredeploy.parameters.json"
 
 if (!$prefix) {
     Write-Host 'Using default name prefix'
@@ -12,8 +14,9 @@ if (!$ResourceGroupName.StartsWith("rg")) {
     $ResourceGroupName = "rg-"+$ResourceGroupName+"-"+$Location
 }
 
-Get-AzResourceGroup -Name $ResourceGroupName -ErrorVariable $notPresent -ErrorAction silentlycontinue
-if (!$notPresent)
+Get-AzResourceGroup -Name $ResourceGroupName -ErrorVariable notPresent -ErrorAction silentlycontinue
+Write-Host $notPresent
+if ($notPresent)
 {
     New-AzResourceGroup -Name $ResourceGroupName -Location $Location
 }
@@ -24,20 +27,20 @@ $deploymentName="WebAppDeploy"+"${today}"
 $DatabasePassword = ConvertTo-SecureString $DatabasePassword  -AsPlainText -Force
 $slackURL = ConvertTo-SecureString $slackURL  -AsPlainText -Force
 
-
-$notValid=Test-AzResourceGroupDeployment -ResourceGroupName $ResourceGroupName -ErrorVariable $notValid -ErrorAction silentlycontinue -TemplateFile $TemplateFile -TemplateParameterFile  $TemplateParameterFile -Location $Location 5>&1 
+Write-Host 3
+$notValid=Test-AzResourceGroupDeployment -ResourceGroupName $ResourceGroupName -ErrorVariable $notValid -ErrorAction SilentlyContinue -TemplateFile $TemplateFile -TemplateParameterFile  $TemplateParameterFile -Location $Location 5>&1 
 if ($notValid) {
     Write-Host $notValid.Message
     Write-Host "Template is not valid according to the validation procedure\n Use Get-AzLog -CorrelationId <correlationId> for more info"
     exit
 }
-
-$alertScript = Get-Content -Path "templates\alertScript.csx" -Raw
+Write-Host 4
+$alertScript = Get-Content -Path ".\alertScript.csx" -Raw
 $alertScript=$alertScript.replace('\\','\\')
 $alertScript=$alertScript.replace('"','\"')
 $alertScript=$alertScript.replace("`r`n",'\r\n')
 
-New-AzResourceGroupDeployment -Force`
+New-AzResourceGroupDeployment `
     -Name $deploymentName -ResourceGroupName $ResourceGroupName -Location $Location `
     -TemplateFile $TemplateFile -TemplateParameterFile  $TemplateParameterFile `
     -prefix $prefix  -databasePassword $databasePassword `
