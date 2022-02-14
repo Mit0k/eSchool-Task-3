00a08be4558faf1de22439e5a38f57d860a9eb27
Mitok Oleksandr <mitok.alexander@gmail.com>
2022-02-14 18:25:53 +0200

    .

--- templates/azuredeploy.ps1
+++ templates/azuredeploy.ps1
@@ -1,7 +1,7 @@
 param($Location, $ResourceGroupName, $DatabasePassword, $TemplateFile, $TemplateParameterFile, $prefix, $slackURL)
 $TemplateFile="templates\azuredeploy.json"
 $TemplateParameterFile="templates\azuredeploy.parameters.json"
-
+$alertScript = Get-Content -Path "templates\alertScript.csx" -Raw
 
 if (!$prefix) {
     Write-Host 'Using default name prefix'
@@ -31,7 +31,7 @@ if ($notValid) {
     Write-Host "Template is not valid according to the validation procedure\n Use Get-AzLog -CorrelationId <correlationId> for more info"
     exit
 }
-$alertScript = Get-Content -Path ".\alertScript.csx" -Raw
+
 $alertScript=$alertScript.replace('\\','\\')
 $alertScript=$alertScript.replace('"','\"')
 $alertScript=$alertScript.replace("`r`n",'\r\n')
