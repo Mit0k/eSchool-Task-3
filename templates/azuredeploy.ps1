1abff276a0fbafaee7d576b3638362c5dd046030
Mitok Oleksandr <mitok.alexander@gmail.com>
2022-02-14 18:18:24 +0200

    '.'

--- templates/azuredeploy.ps1
+++ templates/azuredeploy.ps1
@@ -8,7 +8,7 @@ if (!$prefix) {
     Write-Host 'Using default name prefix'
     $prefix = 'armeschool'
 }
-Write-Host 1
+
 if (!$ResourceGroupName.StartsWith("rg")) {
     $ResourceGroupName = "rg-"+$ResourceGroupName+"-"+$Location
 }
@@ -19,41 +19,25 @@ if ($notPresent)
 {
     New-AzResourceGroup -Name $ResourceGroupName -Location $Location
 }
-Write-Host 3
+
 $today=Get-Date -Format "MM-dd-yyyy-HH-mm"
 $deploymentName="WebAppDeploy"+"${today}"
 
 $DatabasePassword = ConvertTo-SecureString $DatabasePassword  -AsPlainText -Force
 $slackURL = ConvertTo-SecureString $slackURL  -AsPlainText -Force
-<<<<<<< HEAD
 Write-Host "##[debug]Validating template"
 $notValid=Test-AzResourceGroupDeployment -ResourceGroupName $ResourceGroupName -ErrorVariable $notValid -ErrorAction SilentlyContinue -TemplateFile $TemplateFile -TemplateParameterFile  $TemplateParameterFile -Location $Location 5>&1 
-=======
-
-
-$notValid=Test-AzResourceGroupDeployment -ResourceGroupName $ResourceGroupName -ErrorVariable $notValid -ErrorAction silentlycontinue -TemplateFile $TemplateFile -TemplateParameterFile  $TemplateParameterFile -Location $Location -Force 5>&1 
->>>>>>> dfc4364f5068c8b873d54d2586c7758128743f7a
 if ($notValid) {
     Write-Host $notValid.Message
     Write-Host "Template is not valid according to the validation procedure\n Use Get-AzLog -CorrelationId <correlationId> for more info"
     exit
 }
-<<<<<<< HEAD
 $alertScript = Get-Content -Path ".\alertScript.csx" -Raw
 $alertScript=$alertScript.replace('\\','\\')
 $alertScript=$alertScript.replace('"','\"')
 $alertScript=$alertScript.replace("`r`n",'\r\n')
 Write-Host "##[debug]Deploying template"
 New-AzResourceGroupDeployment `
-=======
-Write-Host 4
-$alertScript = Get-Content -Path "templates\alertScript.csx" -Raw
-$alertScript=$alertScript.replace('\\','\\')
-$alertScript=$alertScript.replace('"','\"')
-$alertScript=$alertScript.replace("`r`n",'\r\n')
-Write-Host 5
-New-AzResourceGroupDeployment -Confirm:$false `
->>>>>>> dfc4364f5068c8b873d54d2586c7758128743f7a
     -Name $deploymentName -ResourceGroupName $ResourceGroupName -Location $Location `
     -TemplateFile $TemplateFile -TemplateParameterFile  $TemplateParameterFile `
     -prefix $prefix  -databasePassword $databasePassword `
