a2a77cd16a8d728e55c16e9c0b657cf3bf37e200
Mitok Oleksandr <mitok.alexander@gmail.com>
2022-02-20 18:21:36 +0200

    Fix typo

--- templates/azuredeploy.ps1
+++ templates/azuredeploy.ps1
@@ -30,7 +30,7 @@ $slackURL = ConvertTo-SecureString $slackURL  -AsPlainText -Force
 
 Write-Host "##[debug]Validating template"
 $notValid=Test-AzDeployment -ErrorVariable notValid -ErrorAction SilentlyContinue -TemplateFile $TemplateFile -TemplateParameterFile  $TemplateParameterFile -Location $Location 5>&1 
-if ($notValid) {
+if (!$notValid) {
     Write-Host $notValid.Code
     Write-Host $notValid.Message
     Write-Host $notValid.Details
