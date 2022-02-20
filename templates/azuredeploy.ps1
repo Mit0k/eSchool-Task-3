c66aa2bba29f73b427b630d44b9fd5f8c71869d9
Mitok Oleksandr <mitok.alexander@gmail.com>
2022-02-20 19:31:20 +0200

    Fix typo

--- templates/azuredeploy.ps1
+++ templates/azuredeploy.ps1
@@ -40,6 +40,7 @@ if (!$notValid) {
 
 Write-Host "##[debug]Deploying template"
 New-AzDeployment `
+    -DeploymentDebugLogLevel All
     -Name $deploymentName -Location $Location `
     -TemplateFile $TemplateFile -TemplateParameterFile $TemplateParameterFile `
     -prefix $prefix  -databasePassword $databasePassword `
