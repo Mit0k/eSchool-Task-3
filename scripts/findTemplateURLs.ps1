d2544daf5653c87f485065f8075d53d34cc712f9
Mitok Oleksandr <mitok.alexander@gmail.com>
2022-02-20 16:45:24 +0200

    Fix typo

--- scripts/findTemplateURLs.ps1
+++ scripts/findTemplateURLs.ps1
@@ -13,7 +13,7 @@ $templateUrlList = @()
 
 Foreach ($file in $templateFiles){
     $fullpath="https://api.github.com/repos/$owner/$repo/contents/$dir/$file?ref=$branch"
-    Write-Host "##[debug]Obtaining $fullpath"
+    Write-Host "##[debug]Obtaining $file from $fullpath"
 
     $response = (Invoke-RestMethod $fullpath -Method 'GET' -Headers $headers -Body $body) 
     $templateUrlList.Add($response.download_url)
