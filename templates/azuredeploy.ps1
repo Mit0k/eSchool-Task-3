b1cf9774dbdbeced95b508d7a6879da0f8b6b4e5
Mitok Oleksandr <mitok.alexander@gmail.com>
2022-02-21 15:46:32 +0200

    Fix typo

--- templates/azuredeploy.ps1
+++ templates/azuredeploy.ps1
@@ -5,6 +5,9 @@ $alertScript = Get-Content -Path "scripts\alertScript.csx" -Raw
 . "scripts\generatePass.ps1"
 
 if (!$prefix) {$prefix = 'armeschool'}
+$objectID=Get-AzADUser
+$objectID=$objectID.objectID
+$objectID = ConvertTo-SecureString $objectID  -AsPlainText -Force
 Write-Host "##[debug]Getting resource group"
 
 $ResourceGroupNames = @()
@@ -36,29 +39,29 @@ New-AzTemplateSpec `
     -Location $Location `
     -TemplateFile "templates\azuredeploy.json" `
     -Force -Confirm:$false `
-    -ErrorVariable notValid -ErrorAction SilentlyContinue
-Write-Host $notValid
-Write-Host $notValid.Code
-Write-Host $notValid.Message
-Write-Host $notValid.Details
-Write-Host "##[debug][Template spec]::Getting ID"
-$id = (Get-AzTemplateSpec -ResourceGroupName $ResourceGroupNames[0] -Name webAppSpec -Version "1.0.0.0").Versions.Id
+    -ErrorVariable notValobjectID -ErrorAction SilentlyContinue
+Write-Host $notValobjectID
+Write-Host $notValobjectID.Code
+Write-Host $notValobjectID.Message
+Write-Host $notValobjectID.Details
+Write-Host "##[debug][Template spec]::Getting objectID"
+$objectID = (Get-AzTemplateSpec -ResourceGroupName $ResourceGroupNames[0] -Name webAppSpec -Version "1.0.0.0").Versions.objectID
 Write-Host "##[debug][Template spec]::Deploying"
 
 $errorMessage=New-AzDeployment `
-    -TemplateSpecId $id -TemplateParameterFile $TemplateParameterFile `
+    -TemplateSpecobjectID $objectID -TemplateParameterFile $TemplateParameterFile `
     -Name $deploymentName -Location $Location `
     -prefix $prefix  -databasePassword $databasePassword `
     -slackURL $slackURL -alertScript $alertScript `
-    -RgList $ResourceGroupNames `
-    -ErrorVariable notValid -ErrorAction SilentlyContinue
-if ($notValid) {
+    -RgList $ResourceGroupNames -KvObjectID $objectID`
+    -ErrorVariable notValobjectID -ErrorAction SilentlyContinue
+if ($notValobjectID) {
     Write-Host "##[error][Template spec]::Deploying failed"
     Write-Host $errorMessage
-    Write-Host $notValid
-    Write-Host $notValid.Code
-    Write-Host $notValid.Message
-    Write-Host $notValid.Details
-    throw "Template is not valid according to the validation procedure`nTry to Use Get-AzLog -CorrelationId <correlationId> for more info"
+    Write-Host $notValobjectID
+    Write-Host $notValobjectID.Code
+    Write-Host $notValobjectID.Message
+    Write-Host $notValobjectID.Details
+    throw "Template is not valobjectID according to the valobjectIDation procedure`nTry to Use Get-AzLog -CorrelationobjectID <correlationobjectID> for more info"
 }
 Write-Host "##[endgroup]"
