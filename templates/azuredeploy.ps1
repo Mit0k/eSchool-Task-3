288cb06f51ab55f8b7965b70abc4224d6b593f76
Mitok Oleksandr <mitok.alexander@gmail.com>
2022-03-08 12:46:23 +0200

    KV fix

--- templates/azuredeploy.ps1
+++ templates/azuredeploy.ps1
@@ -14,7 +14,11 @@ if (!$prefix) {$prefix = 'armeschool'}
 $today=Get-Date -Format "MM-dd-yyyy-HH-mm"
 $deploymentName="WebAppDeploy"+"${today}"
 
-$DatabasePassword = ConvertTo-SecureString (Get-RandomPassword 8)  -AsPlainText -Force
+$secret = Get-AzKeyVaultSecret -VaultName 'kv-upser-eastus' -Name 'db-upser-eastusPass'
+if (!$secretText)
+    $DatabasePassword = ConvertTo-SecureString (Get-RandomPassword 8)  -AsPlainText -Force
+else
+    $DatabasePassword = $secret
 $slackURL = ConvertTo-SecureString $slackURL  -AsPlainText -Force
 
 Write-Host "##[debug]Getting resource group"
